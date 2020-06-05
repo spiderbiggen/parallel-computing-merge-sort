2d8a78fe57d7499b89558fc30ab6844ed5b3ca2c
Remco <remcotimmerman1@gmail.com>
2020-06-05 14:39:53 +0200

    Add messaging class

---
+++ src/main/java/computing/parallel/sort/MergeSortMessaging.java
@@ -0,0 +1,10 @@
+package computing.parallel.sort;
+
+import java.util.List;
+
+public class MergeSortMessaging<T extends Comparable<T>> extends MergeSortBase<T> implements Sorter<T> {
+    @Override
+    public List<T> sort(List<T> list) {
+        return null;
+    }
+}
